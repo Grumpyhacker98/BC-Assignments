0ba983044e929f66ed761a40ff1ff273bcf6fddf
Max Lewis <max.a.lewis98@gmail.com>
2020-02-01 11:55:36 -0700

    node calculator

---
+++ 10-node/06-calc.js
@@ -0,0 +1,13 @@
+if(process.argv[2]==="add"){
+    console.log(parseFloat(process.argv[3]) + parseFloat(process.argv[4]))
+}else if(process.argv[2]==="subtract"){
+    console.log(parseFloat(process.argv[3]) - parseFloat(process.argv[4]))
+}else if(process.argv[2]==="multiply"){
+    console.log(parseFloat(process.argv[3]) * parseFloat(process.argv[4]))
+}else if(process.argv[2]==="divide"){
+    console.log(parseFloat(process.argv[3]) / parseFloat(process.argv[4]))
+}else if(process.argv[2]==="remainder"){
+    console.log(parseFloat(process.argv[3]) % parseFloat(process.argv[4]))
+}else{
+    console.log("error")
+}
